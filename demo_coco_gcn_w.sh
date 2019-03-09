2a64d81513bd442fb49f7113ba8bb7d441f9e72c
chenzhaomin <88485740@qq.com>
2019-03-09 14:22:41 +0800

    delete

--- demo_coco_gcn_w.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rlaunch --cpu=15 --gpu=4 --memory=87040 -- python3 demo_coco_gcn.py \
-/data/coco \
---image-size 448 \
---batch-size 64 \
--j 30 \
---resume checkpoint/coco/coco_checkpoint.pth.tar \
--e
